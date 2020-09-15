7c2710eabb9c85470ecc912326afd2097ce10a94
Lajanke <l.j.kenny@hotmail.com>
2020-09-15 12:11:52 +0100

    ArticleList component created.

---
+++ src/components/ArticleList.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ArticleList() {
+  return (
+    <ul>
+        <p>list</p>
+        <p>list</p>
+        <p>list</p>
+    </ul>
+  );
+}
+
+export default ArticleList;
