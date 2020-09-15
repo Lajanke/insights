0348d3144b722b12ac7a5b939766e037cdea9b85
Lajanke <l.j.kenny@hotmail.com>
2020-09-15 12:30:13 +0100

    axios request to retrieve article list.

--- src/components/ArticleList.jsx
+++ src/components/ArticleList.jsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
 
 function ArticleList() {
-  return (
-    <ul>
-        <p>list</p>
-        <p>list</p>
-        <p>list</p>
-    </ul>
-  );
+    const [articleList, setList] = useState([]);
+
+    useEffect(() => {
+        console.log('in effect')
+        Axios.get('http://localhost:8000')
+        .then((res) => {
+            setList(res.data);
+        })
+        .catch((err) => {
+            console.log(err)
+        })
+    }, []);
+
+    console.log(articleList)
+
+    return (
+        <ul>
+            <p>list</p>
+            <p>list</p>
+            <p>list</p>
+        </ul>
+    );
 }
 
 export default ArticleList;
