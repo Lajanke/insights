7d9e3e7a4cccde72d877160f9da2afe81d60f4ce
Lajanke <l.j.kenny@hotmail.com>
2020-09-15 13:36:17 +0100

    alternates transition direction for images.

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,14 +4,15 @@ function ArticleCard(props) {
 
     const { title, link } = props;
     const image = props.image ? `${props.image}` : 'https://kidsinsights.com/wp-content/uploads/2019/09/Globe-Graphic.png';
-
+    const direction = props.num % 2 === 1 ? 'left' : 'right';
+    
     return (
         <div className='articleCard'>
             <img src={`${image}`} 
             alt='related' 
-            data-aos="zoom-in-left"
-            data-aos-offset="150"
-            data-aos-duration="2000"
+            data-aos={`zoom-in-${direction}`}
+            data-aos-offset='150'
+            data-aos-duration='2000'
             />
             <a href={`${link}`}>{`${title}`}<p>(external link)</p></a>
         </div>
